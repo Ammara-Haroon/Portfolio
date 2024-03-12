93523af96b5e9761be76edc7c30cbc838e7f6dc5
Ammara-Haroon <160440578+Ammara-Haroon@users.noreply.github.com>
2024-03-12 22:32:43 +1100

    added toggle funtion to email icon

--- script/script.js
+++ script/script.js
@@ -4,3 +4,25 @@ function renderCopyright() {
 }
 
 renderCopyright();
+
+const emailIcon = document.querySelector("#emailIcon");
+const contactForm = document.querySelector("#contactForm");
+const btnSend = document.querySelector("#submitButton");
+contactForm.style.display = "none";
+
+btnSend.addEventListener("click", () => {
+  if (document.querySelector(":invalid")) {
+    console.log("invalid input");
+    return;
+  }
+  contactForm.style.display = "none";
+});
+
+emailIcon.addEventListener("click", () => {
+  if (contactForm.style.display === "none") {
+    contactForm.style.display = "flex";
+    console.dir(contactForm);
+  } else {
+    contactForm.style.display = "none";
+  }
+});
